76f7e81e533842e108f98bd95137d3ccc409e2d8
kingmario <kingmario@qq.com>
2016-11-28 21:43:12 +0800

    Initial split of components

---
+++ src/components/Supplier.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: []
+  }
+</script>
+
+<style lang="less" rel="stylesheet/less" scoped>
+
+</style>

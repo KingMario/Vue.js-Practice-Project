326a23ee00d01ef43cd19f759bdcfcb9d8f3e335
kingmario <kingmario@qq.com>
2016-12-11 11:05:57 +0800

    Distribution branch

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../../flight-booking-express/views/index.ejs'),
+    assetsRoot: path.resolve(__dirname, '../../flight-booking-express/public'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
